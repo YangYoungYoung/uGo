c04f506eac10289b3d02705a08d14c83ed76c1ba
YangYoungYoung <1249091287@qq.com>
2019-02-17 19:34:50 +0800

    增加了审核模块的部分ui

---
+++ pages/authentication/authentication.js
@@ -0,0 +1,34 @@
+// pages/authentication/authentication.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text:"抱歉，您的资料未通过审核， 请重新提交。",
+    isAuthentication:false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  //重新提交
+  resubmit:function(){
+    
+  }
+
+  
+})
