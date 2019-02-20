19eba06a32a9a30f7198e8e937199e843c081db1
YangYoungYoung <1249091287@qq.com>
2019-02-20 19:47:53 +0800

    修改了支付订单的ui

--- pages/center/center.js
+++ pages/center/center.js
@@ -1,4 +1,5 @@
 // pages/center/center.js
+var app = getApp();
 Page({
 
   /**
@@ -24,20 +25,20 @@ Page({
       }
     ],
     orderList: [{
-        name: "未支付",
-        src: "../images/order_nopay1.png"
+        name: "待确认",
+        src: "../images/order_nopay.png"
       },
       {
-        name: "未发货",
-        src: "../images/order_unshipped1.png"
-      },
-      {
-        name: "已发货",
-        src: "../images/order_shiped1.png"
+        name: "待收货",
+        src: "../images/order_shiped.png"
       },
       {
         name: "已完成",
-        src: "../images/order_done1.png"
+        src: "../images/order_done.png"
+      },
+      {
+        name: "已取消",
+        src: "../images/order_canceled.png"
       },
     ]
   },
@@ -46,7 +47,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo.avatarUrl);
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    // console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
 
@@ -84,16 +119,16 @@ Page({
     })
   },
   //跳转到订单管理
-  toOrder:function(event){
+  toOrder: function(event) {
     let index = event.currentTarget.dataset.index;
-    console.log("index is:",index);
-    
-      wx.navigateTo({
-        url: '../integral_order/integral_order?id='+index,
-      })
+    console.log("index is:", index);
+
+    wx.navigateTo({
+      url: '../integral_order/integral_order?id=' + index,
+    })
   },
   //成为商家
-  toMerchant:function(){
+  toMerchant: function() {
     wx.navigateTo({
       url: '../authorizationStatement/authorizationStatement',
     })
--- pages/integral_payOrder/integral_payOrder.js
+++ pages/integral_payOrder/integral_payOrder.js
@@ -5,8 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 输入框参数设置
+    inputData: {
+      input_value: "", //输入框的初始内容
+      value_length: 0, //输入框密码位数
+      isNext: false, //是否有下一步的按钮
+      get_focus: true, //输入框的聚焦状态
+      focus_class: true, //输入框聚焦样式
+      value_num: [1, 2, 3, 4, 5, 6], //输入框格子数
+      height: "98rpx", //输入框高度
+      width: "604rpx", //输入框宽度
+      see: false, //是否明文展示
+      interval: true, //是否显示间隔格子
+    },
     showAddr: true,
-    showAddAddr:true
+    showAddAddr: true,
+    showModal: false,
+    showPwdModal: false
   },
 
   /**
@@ -45,4 +60,86 @@ Page({
       common.showTip("当前微信版本不支持获取地址", "loading");
     }
   },
+
+  //提交订单
+  payOrder: function() {
+
+    let that = this;
+    that.showModal();
+  },
+  
+  //显示对话框
+  showModal: function() {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModal: true
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  /**
+   * 弹出框蒙层截断touchmove事件
+   */
+  preventTouchMove: function() {},
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function() {
+    this.setData({
+      showModal: false
+    });
+  },
+  //显示密码弹窗
+  // showPwdModal: function() {
+  //   // 显示遮罩层
+  //   var animation = wx.createAnimation({
+  //     duration: 200,
+  //     timingFunction: "linear",
+  //     delay: 0
+  //   })
+  //   this.animation = animation
+  //   animation.translateY(300).step()
+  //   this.setData({
+  //     animationData: animation.export(),
+  //     showPwdModal: true
+  //   })
+  //   setTimeout(function() {
+  //     animation.translateY(0).step()
+  //     this.setData({
+  //       animationData: animation.export()
+  //     })
+  //   }.bind(this), 200)
+  // },
+
+  inputPwd: function() {
+    let that = this;
+    //隐藏兑换弹窗
+    that.hideModal();
+    that.setData({
+      showPwdModal: true
+    });
+  },
+  //隐藏密码弹窗
+  hidePwdModal: function () {
+    this.setData({
+      showPwdModal: false
+    });
+  },
+
+  //忘记密码
+  forgetPwd:function(){
+
+  }
 })
