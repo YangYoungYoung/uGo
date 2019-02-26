8a5629287b2f9d2542220948502b1ebd7379046c
YangYoungYoung <1249091287@qq.com>
2019-02-26 18:44:04 +0800

    增加了手机号验证ui

--- pages/securitySetting/phoneSetting/changePhone/changePhone.js
+++ pages/securitySetting/phoneSetting/changePhone/changePhone.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    clickable:false
   },
 
   /**
@@ -14,53 +14,21 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取手机号
+  phoneInput:function(e){
+    let phoneNumber = e.detail.value;
+    console.log(phoneNumber.length);
+    if (phoneNumber.length==11){
+      console.log(phoneNumber);
+      this.setData({
+        clickable: true
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //下一步
+  nextStep:function(){
+    wx.navigateTo({
+      url: '',
+    })
   }
 })
