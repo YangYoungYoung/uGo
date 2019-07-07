6808334fb6ccadefff7473f8da86e40415d7807f
YangYoungYoung <1249091287@qq.com>
2019-07-07 19:22:29 +0800

    增加了注册登录接口

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,66 +1,114 @@
 // pages/register/register.js
+var network = require("../../utils/network.js")
+var common = require("../../utils/common.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    mobile: '',
+    code: '',
+    pwd:'',
+    integralPwd:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  }, 
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //获取手机号
+  getMobile: function(e) {
+    let mobile = e.detail.value;
+    console.log('mobile is:', mobile);
+    this.setData({
+      mobile: mobile
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //获取验证码
+  getCode: function(e) {
+    let code = e.detail.value;
+    console.log('code is:', code);
+    this.setData({
+      code: code
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  //获取密码
+  getPwd: function (e) {
+    let pwd = e.detail.value;
+    console.log('pwd is:', pwd);
+    this.setData({
+      pwd: pwd
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  //确实密码
+  confirmPwd: function (e) {
+    let confirmPwd = e.detail.value;
+    console.log('confirmPwd is:', confirmPwd);
+    this.setData({
+      confirmPwd: confirmPwd
+    })
   },
+  //获取积分支付密码
+  getIntegralPwd: function (e) {
+    let integralPwd = e.detail.value;
+    console.log('integralPwd is:', integralPwd);
+    this.setData({
+      integralPwd: integralPwd
+    })
+  },
+  //获取积分支付密码
+  confirmIntegralPwd: function (e) {
+    let confirmIntegralPwd = e.detail.value;
+    console.log('confirmIntegralPwd is:', confirmIntegralPwd);
+    this.setData({
+      confirmIntegralPwd: confirmIntegralPwd
+    })
+  },
+  //发送验证码
+  sendCode: function() {
+    let that = this;
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    let mobile = that.data.mobile;
+    if (mobile == undefined || mobile.length != 11 || mobile == '') {
+      common.showTip("请输入正确手机号", "loading");
+      return;
+    }
+    let url = "common/smscode"
+    // ? mobile = + mobile + "&type=2";
+    let method = "GET";
+    var params = {
+      mobile: mobile,
+      type: 1
+    }
+    wx.showLoading({
+        title: '加载中...',
+      }),
+      network.POST(url, params, method).then((res) => {
+        wx.hideLoading();
+        //后台交互
 
+        if (res.data.code == 200) {
+          common.showTip('发送成功', "success");
+        }
+      }).catch((errMsg) => {
+        wx.hideLoading();
+        // console.log(errMsg); //错误提示信息
+        wx.showToast({
+          title: '网络错误',
+          icon: 'loading',
+          duration: 1500,
+        })
+      });
   }
 })
